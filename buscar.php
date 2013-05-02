b4f0ea74ee8738673c0c86bafc4db6158ee4f2f8
Sergio Rodriguez <sergio_ranmos_11@hotmail.com>
2013-05-01 20:19:43 -0500

    buscar.php

--- buscar.php
+++ buscar.php
@@ -1,10 +1,33 @@
 <?php 
 echo "busqueda.php";
 
+<<<<<<< HEAD
 echo "esto es una prueba";
 
 
 $palabra = $_POST['palabra'];
 
+=======
+$archivo = fopen("./jugadores", "w+");
+$arregloJugadores = array();
+
+/**
+class Jugador
+{
+	private $nombre;
+
+	public function ___construct ($nombre)
+	{
+		$this->nombre = $nombre;
+	}
+}
+**/
+while(!feof($archivo))
+{
+	echo fgets($archivo);
+}
+
+fclose($archivo);
+>>>>>>> buscar.php
 
 ?>
